e901f00b529333f1720982de1f746bdb1677df29
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2025-12-08 17:48:02 +0100

    redesigne page

--- src/components/PriceCard/PriceCard.tsx
+++ src/components/PriceCard/PriceCard.tsx
@@ -1,18 +1,36 @@
 function PriceCard() {
   return (
     <div className="price-card d-flex flex-column gap-2">
-      <h4 className="price-card__title fs-5 fw-normal text-center m-0">Базовый</h4>
-      <span className="price-card__price fw-bold fs-1 text-center m-0">400zl</span>
-      <div className="price-card__description">
-        <ul className="fs-6">
-          <li className="fw-light">Диагности</li>
-          <li className="fw-light">Диагности 12</li>
-          <li className="fw-light">Диагно</li>
-        </ul>
+      <h4 className="price-card__title fs-5 fw-normal text-center m-0">
+        Базовый
+      </h4>
+      <span className="price-card__price fw-bold fs-1 text-center m-0">
+        400zl
+      </span>
+      <div className="price-card__description d-flex flex-column gap-4">
+        <div className="price-card__content">
+          <div className="price-card__item-content d-flex">
+            <div className="price-card__chech-icon"></div>
+            <span className="fw-light">Диагности</span>
+          </div>
+          <div className="price-card__item-content ">
+            <div className="price-card__chech-icon"></div>
+            <span className="fw-light">Диагности</span>
+          </div>
+          <div className="price-card__item-content ">
+            <div className="price-card__chech-icon"></div>
+            <span className="fw-light">Диагности</span>
+          </div>
+          <div className="price-card__item-content ">
+            <div className="price-card__chech-icon"></div>
+            <span className="fw-light">Диагности</span>
+          </div>
+        </div>
+
         <button className="price-card__btn w-100">Заказать</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default PriceCard
+export default PriceCard;
--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -4,158 +4,71 @@ const Services = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="services" id='services'>
-      <div className="container d-flex flex-column gap-3 services__container">
-        <h2 className="fs-1 services__title">{t('services.title')}</h2>
-        <div className="accordion" id="accordionExample">
-          <div className="accordion-item">
-            <h2 className="accordion-header">
-              <button
-                className="accordion-button d-flex gap-2 align-items-center justify-content-center"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne"
-              >
-                <div className="icon icon--key"></div>
-                <p className="m-0 fs-4">{t('services.accordion.sectionOne.title')}</p>
-              </button>
-            </h2>
-            <div
-              id="collapseOne"
-              className="accordion-collapse collapse show"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                {t('services.accordion.sectionOne.description')}
-              </div>
-            </div>
+    <div className="services" id="services">
+      <div className="container d-flex flex-column gap-5 services__container">
+        <h2 className="fs-1 services__title text-center">
+          {t('services.title')}
+        </h2>
+        {/* <div className="row row-cols-1 row-cols-sm-6 row-cols-md-3 row-cols-lg-4 gap-4 justify-content-center"> */}
+        <div className="row gap-4 justify-content-center">
+          <div className="services__block col-12 col-sm-6 col-md-3 d-flex flex-column justify-content-between">
+            <div className="services__icon icon--car"></div>
+            <h3 className="services__title-block">
+              {t('services.accordion.sectionOne.title')}
+            </h3>
+            <p className="services__description">
+              {t('services.accordion.sectionOne.description')}
+            </p>
           </div>
 
-          <div className="accordion-item">
-            <h2 className="accordion-header">
-              <button
-                className="accordion-button collapsed d-flex gap-2 align-items-center justify-content-center"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseTwo"
-                aria-expanded="false"
-                aria-controls="collapseTwo"
-              >
-                <div className="icon icon--repeat-once"></div>
-                <p className="m-0 fs-4">{t('services.accordion.sectionTwo.title')}</p>
-              </button>
-            </h2>
-            <div
-              id="collapseTwo"
-              className="accordion-collapse collapse"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                {t('services.accordion.sectionTwo.description')}
-              </div>
-            </div>
+          <div className="services__block col-12 col-sm-6 col-md-3 d-flex flex-column justify-content-between">
+            <div className="services__icon icon--one-time"></div>
+            <h3 className="services__title-block">
+              {t('services.accordion.sectionTwo.title')}
+            </h3>
+            <p className="services__description">
+              {t('services.accordion.sectionTwo.description')}
+            </p>
           </div>
 
-          <div className="accordion-item">
-            <h2 className="accordion-header">
-              <button
-                className="accordion-button collapsed d-flex gap-2 align-items-center justify-content-center"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseThree"
-                aria-expanded="false"
-                aria-controls="collapseThree"
-              >
-                <div className="icon icon--car-diagnostics"></div>
-                <p className="m-0 fs-4">{t('services.accordion.sectionThree.title')}</p>
-              </button>
-            </h2>
-            <div
-              id="collapseThree"
-              className="accordion-collapse collapse"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                {t('services.accordion.sectionThree.description')}
-              </div>
-            </div>
+          <div className="services__block col-12 col-sm-6 col-md-3 d-flex flex-column justify-content-between">
+            <div className="services__icon icon--before-purchase-icon"></div>
+            <h3 className="services__title-block">
+              {t('services.accordion.sectionThree.title')}
+            </h3>
+            <p className="services__description">
+              {t('services.accordion.sectionThree.description')}
+            </p>
           </div>
 
-          <div className="accordion-item">
-            <h2 className="accordion-header">
-              <button
-                className="accordion-button collapsed d-flex gap-2 align-items-center justify-content-center"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseFour"
-                aria-expanded="false"
-                aria-controls="collapseFour"
-              >
-                <div className="icon icon--money"></div>
-                <p className="m-0 fs-4">{t('services.accordion.sectionFour.title')}</p>
-              </button>
-            </h2>
-            <div
-              id="collapseFour"
-              className="accordion-collapse collapse"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                {t('services.accordion.sectionFour.description')}
-              </div>
-            </div>
+          <div className="services__block col-12 col-sm-6 col-md-3 d-flex flex-column justify-content-between">
+            <div className="services__icon icon--documents"></div>
+            <h3 className="services__title-block">
+              {t('services.accordion.sectionFour.title')}
+            </h3>
+            <p className="services__description">
+              {t('services.accordion.sectionFour.description')}
+            </p>
           </div>
 
-          <div className="accordion-item">
-            <h2 className="accordion-header">
-              <button
-                className="accordion-button collapsed d-flex gap-2 align-items-center justify-content-center"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseFive"
-                aria-expanded="false"
-                aria-controls="collapseFive"
-              >
-                <div className="icon icon--verification"></div>
-                <p className="m-0 fs-4">{t('services.accordion.sectionFive.title')}</p>
-              </button>
-            </h2>
-            <div
-              id="collapseFive"
-              className="accordion-collapse collapse"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                {t('services.accordion.sectionFive.description')}
-              </div>
-            </div>
+          <div className="services__block col-12 col-sm-6 col-md-3 d-flex flex-column justify-content-between">
+            <div className="services__icon icon--online-consultation"></div>
+            <h3 className="services__title-block">
+              {t('services.accordion.sectionFive.title')}
+            </h3>
+            <p className="services__description">
+              {t('services.accordion.sectionFive.description')}
+            </p>
           </div>
 
-          <div className="accordion-item">
-            <h2 className="accordion-header">
-              <button
-                className="accordion-button collapsed d-flex gap-2 align-items-center justify-content-center"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseSix"
-                aria-expanded="false"
-                aria-controls="collapseSix"
-              >
-                <div className="icon icon--support"></div>
-                <p className="m-0 fs-4">{t('services.accordion.sectionSix.title')}</p>
-              </button>
-            </h2>
-            <div
-              id="collapseSix"
-              className="accordion-collapse collapse"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                {t('services.accordion.sectionSix.description')}
-              </div>
-            </div>
+          <div className="services__block col-12 col-sm-6 col-md-3 d-flex flex-column justify-content-between">
+            <div className="services__icon icon--cost-assessment"></div>
+            <h3 className="services__title-block">
+              {t('services.accordion.sectionSix.title')}
+            </h3>
+            <p className="services__description">
+              {t('services.accordion.sectionSix.description')}
+            </p>
           </div>
         </div>
       </div>
